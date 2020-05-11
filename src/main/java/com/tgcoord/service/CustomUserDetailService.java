3bc82b45107c04bb4cd7d613d6e18ae3b2402c9d
Natalia <nataliadognani793@gmail.com>
2020-05-11 10:47:06 -0300

    com.tgcood.service CustomUserDetailService.java

--- src/main/java/com/tgcoord/service/CustomUserDetailService.java
+++ src/main/java/com/tgcoord/service/CustomUserDetailService.java
@@ -11,6 +11,7 @@ import com.tgcoord.repository.FuncionarioRepository;
 import com.tgcoord.repository.GestorRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -31,27 +32,40 @@ public class CustomUserDetailService implements UserDetailsService {
     
     private final FuncionarioRepository funcRep;
 
+    /**
+     *
+     * @param gestRep
+     * @param funcRep
+     */
     @Autowired
     public CustomUserDetailService(GestorRepository gestRep, FuncionarioRepository funcRep) {
+        super();
         this.gestRep = gestRep;
         this.funcRep = funcRep;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public final UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         
-        //O username é o próprio RG do Funcionario/Gestor
+        //O username é o próprio E-mail do Funcionario/Gestor
+        Funcionario funcUser = Optional.ofNullable(funcRep.findByRg(username)).orElseThrow(()-> new UnsupportedOperationException("RG inválido."));
+        System.out.println("Funcionario: " + funcUser.toString());
         
-        Funcionario func = Optional.ofNullable(funcRep.findByRg(username)).orElseThrow(()-> new UnsupportedOperationException("RG inválido."));
-        System.out.println("Funcionari0: ");
-        
-        Optional<Gestor> gest = gestRep.findById(func.getId());
+        Optional<Gestor> gest = gestRep.findById(funcUser.getId());
         System.out.println("Gestor: " + gest.toString());
         
-        List<GrantedAuthority> authorityListAdmin = AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN");               List<GrantedAuthority> authorityListUser = AuthorityUtils.createAuthorityList("ROLE_USER");
+        List<GrantedAuthority> authorityListAdmin = AuthorityUtils.createAuthorityList("ROLE_USER", "ROLE_ADMIN");
+        List<GrantedAuthority> authorityListUser = AuthorityUtils.createAuthorityList("ROLE_USER");
         
         //Se for gestor ele terá acesso admin, se não, terá apenas acesso user
-        return new User(func.getRg(), func.getSenha(), gest.isPresent() ? authorityListAdmin : authorityListUser);
+//        return new User(func.getRg(), func.getSenha(), gest.isPresent() ? authorityListAdmin);
+
+        if(gest.isEmpty()) {
+            return new User(funcUser.getRg(), funcUser.getRg(), authorityListUser);
+        } else {
+            return new User(funcUser.getRg(), funcUser.getRg(), authorityListAdmin);
+        }
     }
+    private static final Logger LOG = Logger.getLogger(CustomUserDetailService.class.getName());
     
 }
