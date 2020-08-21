aa77057d91977b71a30cf21bf644dd23a11f1cc7
NatBrisa <nataliadognani793@gmail.com>
2020-08-21 20:49:04 -0300

    Add files via upload

---
+++ Academia/src/main/java/com/projetoetec/academia/models/Alunos.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projetoetec.academia.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author natal
+ */
+@Entity
+public class Alunos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(length = 100)
+    private String nome;
+    
+    @Column
+    private LocalDate dtNasc;
+    
+    @Column
+    private float peso;
+    
+    @Column
+    private float altura;
+    
+    @Column
+    private String telefone;
+    
+    //plano
+
+    public Alunos() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDtNasc() {
+        return dtNasc;
+    }
+
+    public void setDtNasc(LocalDate dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alunos)) {
+            return false;
+        }
+        Alunos other = (Alunos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.projetoetec.academia.models.Alunos[ id=" + id + " ]";
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+}
